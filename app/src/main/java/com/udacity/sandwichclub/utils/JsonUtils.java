c6aaff8c2023ce5d5e4fa0f0755bd83b6272aea4
Vivek Lingayat <vvk.vivek.v@gmail.com>
2018-06-03 15:18:09 +0530

    Added Json Parse.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,52 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    private static final String NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String DESCRIPTION = "description";
+    private static final String IMG = "image";
+    private static final String INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
+
+        try {
+            JSONObject data = new JSONObject(json);
+            JSONObject name = data.getJSONObject(NAME);
+            String mainName = name.getString(MAIN_NAME);
+            JSONArray alsoKnownAs = name.getJSONArray(ALSO_KNOWN_AS);
+
+            List<String> alsoKnownAsList = new ArrayList<>();
+            for (int i = 0; i < alsoKnownAs.length(); i++) {
+                alsoKnownAsList.add(alsoKnownAs.getString(i));
+            }
+
+            String placeOfOrigin = data.getString(PLACE_OF_ORIGIN);
+            String description = data.getString(DESCRIPTION);
+            String image = data.getString(IMG);
+            JSONArray ingredients = data.getJSONArray(INGREDIENTS);
+
+            List<String> ingredientsList = new ArrayList<>();
+            for (int i = 0; i < ingredients.length(); i++) {
+                ingredientsList.add(ingredients.getString(i));
+            }
+
+            Sandwich sandwich = new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientsList);
+            return sandwich;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
